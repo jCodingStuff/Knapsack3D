fd1d5284c6335b7bc2128b4fcee8c2b2a626f748
DESKTOP-58UKOAJ\Sarah <s.waseem@student.maastrichtuniversity.nl>
2018-01-08 19:31:21 +0100

    Added CargoCloning method

---
+++ CloneCargo.java
@@ -0,0 +1,18 @@
+/*
+A method that clones from Cargo
+@author Sarah Waseem
+@author Pierre Bongrad
+@version 0.1, 08 December 2018
+*/
+public static Item[][][] CloneCargo(Item[][][] ori) {
+
+  Item [][][] myInt = new Item [ori.length][ori[0].length][ori[0][0].length];
+  for(int i = 0; i< ori.length; i++){
+      for (int j = 0; j < ori[i].length; j++){
+          for (int k = 0; k < ori[i][j].length; k++){
+              myInt[i][j][k] = ori[i][j][k].clone();
+          }
+      }
+  }
+  return myInt;
+}
