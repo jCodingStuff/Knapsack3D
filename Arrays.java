656d9679dd03577b1a1555b2f3b61db8b41d4bce
Julián Marrades <julian.marrades@hotmail.es>
2018-01-09 12:09:20 +0100

    Added method to clone an array of items

--- Arrays.java
+++ Arrays.java
@@ -7,8 +7,16 @@ A method that clones from Cargo
 
 public class Arrays {
 
+  public static Item[] cloneArray(Item[] ori) {
+    Item[] fut = new Item[ori.length];
+    for (int i = 0; i < fut.length; i++) {
+      fut[i] = ori[i].clone();
+    }
+    return fut;
+  }
+
   public static Item[][][] clone3DMatrix(Item[][][] ori) {
-    Item [][][] myInt = new Item [ori.length][ori[0].length][ori[0][0].length];
+    Item[][][] myInt = new Item [ori.length][ori[0].length][ori[0][0].length];
     for(int i = 0; i< ori.length; i++){
         for (int j = 0; j < ori[i].length; j++){
             for (int k = 0; k < ori[i][j].length; k++){
