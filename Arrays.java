3e10db5dba48eb07622215bf745882bad299dec4
Julián Marrades <julian.marrades@hotmail.es>
2018-01-09 11:48:16 +0100

    Put the method in the class

--- Arrays.java
+++ Arrays.java
@@ -4,14 +4,19 @@ A method that clones from Cargo
 @author Pierre Bongrad
 @version 0.1, 08 December 2018
 */
-public static Item[][][] clone3DMatrix(Item[][][] ori) {
-  Item [][][] myInt = new Item [ori.length][ori[0].length][ori[0][0].length];
-  for(int i = 0; i< ori.length; i++){
-      for (int j = 0; j < ori[i].length; j++){
-          for (int k = 0; k < ori[i][j].length; k++){
-              myInt[i][j][k] = ori[i][j][k].clone();
-          }
-      }
+
+public class Arrays {
+
+  public static Item[][][] clone3DMatrix(Item[][][] ori) {
+    Item [][][] myInt = new Item [ori.length][ori[0].length][ori[0][0].length];
+    for(int i = 0; i< ori.length; i++){
+        for (int j = 0; j < ori[i].length; j++){
+            for (int k = 0; k < ori[i][j].length; k++){
+                myInt[i][j][k] = ori[i][j][k].clone();
+            }
+        }
+    }
+    return myInt;
   }
-  return myInt;
+
 }
