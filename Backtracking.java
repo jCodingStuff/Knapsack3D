45ed0cf84301117637b79b513d6bc2db123c3794
Julián Marrades <julian.marrades@hotmail.es>
2018-01-11 10:40:26 +0100

    Backtracking class created

---
+++ Backtracking.java
@@ -0,0 +1,13 @@
+/**
+* A class with a backtracking algorithm that tries to fill the total volume of
+* a cargo
+*
+* @author Julián Marrades
+* @version 0.1, 11-01-2018
+*/
+
+public class Backtracking {
+
+
+
+}
