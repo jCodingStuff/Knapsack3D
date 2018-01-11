8b3406d79c2d367ec1748ec0c5c500802c2634c9
Julián Marrades <julian.marrades@hotmail.es>
2018-01-11 13:36:27 +0100

    Removed cargo parameter from solveFor and added isFull function

--- Backtracking.java
+++ Backtracking.java
@@ -8,8 +8,33 @@
 
 public class Backtracking {
 
-  public static void solveFor(Item[] item, boolean[] used, Item[][][] shape, Cargo cargo) {
-    
+  public static void solveFor(Item[] item, boolean[] used, Item[][][] shape) {
+
+  }
+
+  /**
+  * Determine if a 3D Item array is full or not
+  * @param shape the 3D array to check
+  * @return true if it is full, false otherwise
+  */
+  public static boolean isFull(Item[][][] shape) {
+    boolean full = true;
+    int i = 0;
+    while (full && i < shape.length) {
+      int j = 0;
+      while (full && j < shape[i].length) {
+        int k = 0;
+        while (full && k < shape[i][j].length) {
+          if (shape[i][j][k] == null) {
+            full = false;
+          }
+          k++;
+        }
+        j++;
+      }
+      i++;
+    }
+    return full;
   }
 
 }
