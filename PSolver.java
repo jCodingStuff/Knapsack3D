7a3a4c2e7e430883d70bddfff18e9e771b13ad16
Julián Marrades <julian.marrades@hotmail.es>
2018-01-17 16:44:23 +0100

    Created PSolver, a solver for pentominoes

---
+++ PSolver.java
@@ -0,0 +1,134 @@
+public class PSolver {
+
+  private Pentomino[] pentos;
+  private Cargo cargo;
+  private String name;
+
+  /**
+  * Default Solver constructor
+  * @param name the name of the solver
+  * @param pentos the array of items
+  * @param cargo the cargo object
+  */
+  public PSolver(String name, Pentomino[] pentos, Cargo cargo)
+  {
+    this.name = name;
+    this.pentos = pentos;
+    this.cargo = cargo;
+  }
+
+  /**
+  * Get access to the name
+  * @return the name of the solver
+  */
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+  * Set a new name for the solver
+  * @param name the new name
+  */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+  * Get access to the items
+  * @return the items
+  */
+  public Pentomino[] getPentos()
+  {
+    return this.pentos;
+  }
+  /**
+  * Sets the items
+  * @param items the new array of items
+  */
+  public void setPentos(Pentomino[] pentos)
+  {
+    this.pentos = pentos;
+  }
+  /**
+  * Gets access to cargo
+  * @return cargo
+  */
+  public Cargo getCargo()
+  {
+    return this.cargo;
+  }
+  /**
+  * Sets cargo
+  * @param cargo the new cargo
+  */
+  public void setCargo(Cargo cargo)
+  {
+    this.cargo = cargo;
+  }
+
+  /**
+  * Get a string representation
+  * @return a string containing the detailed information
+  */
+  public String toString()
+  {
+    String result = this.getClass().getName() + ":\n";
+    result += " - Name -> " + this.name;
+    result += "\n - Cargo -> " + this.cargo.toString();
+    result += "\n - Pentominoes:";
+    for (Pentomino pento : this.pentos) {
+      result += "\n\t- " + pento.toString();
+    }
+    return result;
+  }
+
+  /**
+  * Clone the current Solver
+  * @return the cloned solver object
+  */
+  public PSolver clone() {
+    Pentomino[] newPentos = Arrays.clonePArray(this.pentos);
+    return new PSolver(this.name, newPentos, this.cargo.clone());
+  }
+
+  /**
+  * Fill the cargo with the greedy algorithm
+  */
+  public void fillGreedyCargo() {
+    // Item[] sorted = Item.jSort(this.items);
+    Pentomino[] sorted = Pentomino.getAllShapes(this.pentos);
+    // for (Item item : this.items) System.out.println(item);
+    // Loop through the whole cargo and fill any empty space
+    for (int j = 0; j < this.cargo.getHeight(); j++) {
+      for (int i = 0; i < this.cargo.getWidth(); i++) {
+        for (int k = 0; k < this.cargo.getDepth(); k++) {
+          if (this.cargo.check(i, j, k) == null) {
+            this.tryFill(sorted, i, j, k);
+          }
+        }
+      }
+    }
+    Backtracking.print3DArray(this.cargo.getShape());
+    this.cargo.printSolution(Arrays.toItemArray(this.pentos));
+  }
+
+  /**
+  * Try to fit an item from a set in a position
+  * @param pentos the set of pentos which can be put in the cargo
+  * @param i the position along the x-axis
+  * @param j the position along the y-axis
+  * @param k the position along the z-axis
+  */
+  private void tryFill(Pentomino[] pentos, int i, int j, int k) {
+    boolean filled = false;
+    int index = 0;
+    while (!filled && index < pentos.length) {
+      if (this.cargo.canBePut(pentos[index], i, j, k)) {
+        this.cargo.put(pentos[index], i, j, k);
+        filled = true;
+      }
+      index++;
+    }
+  }
+
+}
