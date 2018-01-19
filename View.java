04d75d08342c6721d263e104e461cdf49db0882f
Lucas Uberti-Bona <lgubm23@gmail.com>
2018-01-19 06:34:19 +0100

    Prepared View for solution

--- View.java
+++ View.java
@@ -9,38 +9,52 @@ import javafx.scene.control.*;
 import javafx.scene.paint.*;
 
 public class View extends Application {
-	
+		public static void main (String[] args) {
+			View test = new View();
+
+		}
+
 		final int SIDE = 40;
-	
+
 		public void start(final Stage stage) {
 			stage.setTitle("Project 1.4 - Cargo");
-			
+
 			Group rotationGroup = new Group();
 			Group root = new Group();
 			Scene scene = new Scene(root, 600, 600, true);
 			scene.setFill(Color.ALICEBLUE);
-			
-			addToRoot(..., rotationGroup, root);
+
+			Item A = new Item("A", 3, 2, 2, 4);
+			Item B = new Item("B", 4, 2, 3, 4);
+			Item C = new Item("C", 5, 3, 3, 3);
+			Item[] items = new Item[]{A, B, C};
+
+			Item[][][] cargo = new Item[2][5][8];
+
+			addToRoot(items, cargo, rotationGroup, root);
 			addSlider(rotationGroup, root);
-			
+
 			scene.setCamera(new PerspectiveCamera());
 			stage.setScene(scene);
 			stage.show();
 		}
-		
+
 		/*
 		 * Adding everything to the rotation group
 		 */
-		public void addToRoot(Item[] items, Cargo cargo, Group rotationGroup, Group root) {
-			
-			<here goes the solver, with items as items to use and cargo as cargo settings>
-		    
-			
-			addBoxes(rotationGroup, <here goes the solution matrix>);
+		public void addToRoot(Item[] items, Item[][][] cargo, Group rotationGroup, Group root) {
+
+			// <here goes the solver, with items as items to use and cargo as cargo settings>
+			Backtracking.solveFor(items, cargo);
+			Cargo tmp = new Cargo("TMP", Backtracking.tmp.getShape());
+			tmp.printSolution(items, false);
+
+
+			addBoxes(rotationGroup, tmp.getShape());
 			setupRG(rotationGroup);
 			root.getChildren().add(rotationGroup);
 		}
-		
+
 		/*
 		 * Create a box that corresponds to a specific item
 		 */
@@ -52,8 +66,8 @@ public class View extends Application {
 			box.setMaterial(mat);
 			return box;
 		}
-		
-		
+
+
 		public void addSlider(Group rotationGroup, Group root) {
 			Slider s = new Slider(0,360,0);
 			s.setBlockIncrement(1);
@@ -62,14 +76,14 @@ public class View extends Application {
 			rotationGroup.rotateProperty().bind(s.valueProperty());
 			root.getChildren().add(s);
 		}
-		
+
 		/*
 		 * Add the boxes to the rotation group
 		 * @param rotationGroup The group the items are added to
 		 * @param items The solution matrix to retrieve the items from
 		 */
 		public void addBoxes(Group rotationGroup, Item[][][] items) {
-			
+
 			for (int i = 0; i < items.length; i++) {
 				for (int j = 0; j < items[i].length; j++) {
 					for (int k = 0; k < items[i][j].length; k++) {
@@ -80,7 +94,7 @@ public class View extends Application {
 				}
 			}
 		}
-		
+
 		/*
 		 * Set the coordinates of a box
 		 * @param Box of which to set the coordinates
@@ -93,7 +107,7 @@ public class View extends Application {
 			box.setTranslateY(y*SIDE);
 			box.setTranslateZ(z*SIDE);
 		}
-		
+
 		/*
 		 * Set up the rotation group
 		 * @param rotationGroup The rotation group to set up
