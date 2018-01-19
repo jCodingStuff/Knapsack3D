bd942cb7c20d903a73dbf60023131ac921b3a60f
Lucas Uberti-Bona <lgubm23@gmail.com>
2018-01-19 06:17:08 +0100

    Test changes

--- Test.java
+++ Test.java
@@ -4,30 +4,30 @@ import java.io.IOException;
 public class Test {
 
   public static void main(String[] args) {
-
-    FileWriter writer = null;
-
-    try {
-      writer = new FileWriter("results.txt");
-    }
-    catch (Exception e) {
-      System.out.println("You are dumb!");
-      System.exit(0);
-    }
-
-    // Item A = new Item("A", 3, 2, 2, 4);
-    // Item B = new Item("B", 4, 2, 3, 4);
-    // Item C = new Item("C", 5, 3, 3, 3);
     //
-    // Item[] items = new Item[]{A, B, C};
-    // Item[] sorted = Item.jSort(items);
+    // FileWriter writer = null;
+    //
+    // try {
+    //   writer = new FileWriter("results.txt");
+    // }
+    // catch (Exception e) {
+    //   System.out.println("You are dumb!");
+    //   System.exit(0);
+    // }
+
+    Item A = new Item("A", 3, 2, 2, 4);
+    Item B = new Item("B", 4, 2, 3, 4);
+    Item C = new Item("C", 5, 3, 3, 3);
+
+    Item[] items = new Item[]{A, B, C};
+    Item[] sorted = Item.jSort(items);
     // for (Item item : sorted) System.out.println(item.getName());
     // Item[] items = new Item[]{A, B};
-    Pentomino L = new Pentomino("L", 3);
-    Pentomino P = new Pentomino("P", 4);
-    Pentomino T = new Pentomino("T", 5);
-
-    Pentomino[] items = new Pentomino[] {L, P, T};
+    // Pentomino L = new Pentomino("L", 3);
+    // Pentomino P = new Pentomino("P", 4);
+    // Pentomino T = new Pentomino("T", 5);
+    //
+    // Pentomino[] items = new Pentomino[] {L, P, T};
 
     // Item[][][] cargo = new Item[33][5][8];
     int width = Integer.parseInt(args[0]);
@@ -36,33 +36,33 @@ public class Test {
     Item[][][] cargo = new Item[width][height][depth];
     // Backtracking.print3DArray(cargo);
 
-    // Backtracking.solveFor(sorted, cargo);
-    // if (Backtracking.tmp != null) {
-    //   Cargo tmp = new Cargo("TMP", Backtracking.tmp.getShape());
-    //   tmp.printSolution(items, false);
+    Backtracking.solveFor(sorted, cargo);
+    if (Backtracking.tmp != null) {
+      Cargo tmp = new Cargo("TMP", Backtracking.tmp.getShape());
+      tmp.printSolution(items, false);
+    }
+    else {
+      System.out.println("The cargo could not be filled :(");
+    }
+    // for (int i = 0; i < 20_000; i++) {
+    //   PSolver mine = new PSolver("Greedy", items, new Cargo("Cargo", width, height, depth));
+    //   mine.fillGreedyCargo();
+    //   if (mine.getCargo().getValue() > 238) {
+    //     try {
+    //       writer.write("\n" + mine.getCargo().getResult());
+    //     }
+    //     catch (IOException e) {
+    //       System.out.println("Motherfucker you sick dumb ass get my try!");
+    //     }
+    //   }
     // }
-    // else {
-    //   System.out.println("The cargo could not be filled :(");
+    //
+    // try {
+    //   writer.close();
     // }
-    for (int i = 0; i < 20_000; i++) {
-      PSolver mine = new PSolver("Greedy", items, new Cargo("Cargo", width, height, depth));
-      mine.fillGreedyCargo();
-      if (mine.getCargo().getValue() > 238) {
-        try {
-          writer.write("\n" + mine.getCargo().getResult());
-        }
-        catch (IOException e) {
-          System.out.println("Motherfucker you sick dumb ass get my try!");
-        }
-      }
-    }
-
-    try {
-      writer.close();
-    }
-    catch (Exception e) {
-      System.out.println("Could not close the writer sdoignarewoignaeriogn");
-    }
+    // catch (Exception e) {
+    //   System.out.println("Could not close the writer sdoignarewoignaeriogn");
+    // }
 
   }
 
