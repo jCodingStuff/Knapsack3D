ac933b5b13df3fa8cfde914a643d455dcb9442c7
Julián Marrades <julian.marrades@hotmail.es>
2018-01-20 11:18:03 +0100

    Now you can indicate the greedy if you want it to be random

--- Solver.java
+++ Solver.java
@@ -107,13 +107,19 @@ public class Solver
 
   /**
   * Fill the cargo with the greedy algorithm
+  * @param random is random filling wanted?
   */
-  public void fillGreedyCargo() {
-    // Item[] sorted = Item.jSort(this.items);
-    Item[] sorted = Item.getAllShapes(this.items);
-    // for (Item item : this.items) System.out.println(item);
-    // Backtracking.printArray(shuffle(sorted));
-    Item[] all = shuffle(sorted);
+  public void fillGreedyCargo(boolean random) {
+    Item[] sorted = null;
+    Item[] all = null;
+    if (random) {
+      sorted = Item.getAllShapes(this.items);
+      all = shuffle(sorted);
+    }
+    else {
+      sorted = Item.jSort(this.items);
+      all = Item.getAllShapes(sorted);
+    }
     // Loop through the whole cargo and fill any empty space
     for (int j = 0; j < this.cargo.getHeight(); j++) {
       for (int i = 0; i < this.cargo.getWidth(); i++) {
